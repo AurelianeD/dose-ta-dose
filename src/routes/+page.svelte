545a3ac0e85a8e4c9714ee1dc67939be134f67bd
Viturna <viturnapro@gmail.com>
2024-04-08 16:40:29 +0200

    ButtonQuiz

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,11 +3,17 @@
 	import '$lib/styles/styles.css';
   import Header from '$lib/components/Header.svelte';
   import Footer from '$lib/components/Footer.svelte';
+  import QuizButton from '$lib/components/QuizButton.svelte';
 </script>
 
 <Header />
 
 <h1>Welcome to Dose ta dose</h1>
+<div class="flex">
+<QuizButton letter="a" answer="Boire beaucoup et de manière régulière 1" />
+<QuizButton letter="b" answer="Réponse 2" />
+<QuizButton letter="c" answer="Boire beaucoup et de manière régulière" />
+</div>
 <button on:click={() => goto('game')}>Le jeu (inviduel)</button>
 <button on:click={() => goto('gameRessources')}>Les ressources du jeu</button>
 <button on:click={() => goto('gamePresentation')}>Le jeu (présentation)</button>
@@ -16,3 +22,15 @@
 <button on:click={() => goto('ressources')}>Ressources</button>
 
 <Footer/>
+<style>
+  .flex{
+    display: flex;
+    padding: 0 5vw;
+    gap: 24px;
+  }
+  @media screen and (max-width: 1000px) {
+    .flex{
+      flex-direction: column;
+    }
+  }
+</style>
