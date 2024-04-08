3c2ac0fafc599fd714ac9cde78d50c5136eb4689
AurelianeD <aureliane.dor@gmail.com>
2024-04-08 11:05:47 +0200

    add component MainButton

---
+++ src/lib/components/MainButton.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    import '$lib/styles/fonts.css'
+</script>
+
+<button class="container main-button">
+    <slot />
+</button>
+
+<style>
+    .container{
+        border: none;
+        background-color: var(--yellow);
+        padding: 12px 32px;
+        border-radius: 8px;
+    }
+    .container:hover{
+        background-color: var(--yellow-hover);
+    }
+    .container:active{
+        background-color: var(--yellow-clicked);
+    }
+</style>
