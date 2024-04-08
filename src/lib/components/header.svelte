ccaade0d90d5c07eec6c88328e86407369a9adf2
LineCab <line.cabaret@mmibordeaux.com>
2024-04-08 14:10:12 +0200

    header en mobile

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -7,7 +7,7 @@
         padding: 10px 5vw;
     }
 
-    hr{
+    .ligne{
         width: 90%;
         border: 1px solid var(--black);
     }
@@ -20,6 +20,7 @@
     .nav-style{
         background: none;
         border: none;
+        line-height: 100%;
         font-family: 'DM-Sans', sans-serif;
         font-weight: 600;
         color: var(--black);
@@ -54,22 +55,103 @@
         cursor: pointer;
     }
 
+    .mobile{
+        display: none;
+    }
+
+    .mobile-nav{
+        display: none;
+    }
+
+    @media screen and (max-width: 1000px){
+
+        .mobile{
+            display: flex;
+            align-items: center;
+            gap: 6px;
+            background: none;
+            border: none;
+            font-family: 'DM-Sans', sans-serif;
+            font-weight: 600;
+            color: var(--black);
+            font-size: 16px;
+            text-transform: uppercase;
+        }
+
+        .desktop-nav{
+            display: none;
+        }
+
+        .mobile-nav{
+            display: flex;
+            flex-direction: column;
+            margin-top: 30px;
+            position: fixed;
+            top: 70px;
+            width: 100%;
+            background-color: var(--white);
+            opacity: 0.95;
+            z-index: 1000;
+        }
+
+        hr{
+            width: 60%;
+            border: 1px solid var(--black);
+        }
+
+    }
+
 </style>
 
 <script>
     import { goto } from "$app/navigation";
-    import '$lib/styles/styles.css'
+    import { onMount } from "svelte";
+
+    let isMenuOpen = false;
+    let arrowSvg = "/static/images/fleche-bas.svg";
+
+    function toggleMenu(){
+        isMenuOpen = !isMenuOpen;
+        arrowSvg = isMenuOpen ? "/static/images/fleche-haut.svg" : "/static/images/fleche-bas.svg";
+    }
+
+    function handleResize() {
+        if (window.innerWidth > 1000) {
+            isMenuOpen = false;
+        }
+    }
+
+    onMount(() => {
+        // Vérifier si window est défini (pas disponible côté serveur)
+        if (typeof window !== "undefined") {
+            window.addEventListener('resize', handleResize);
+        }
+    });
 </script>
 
 <header>
     <div class="flex-header">
         <img src="/static/images/logo.svg" alt="logo dose ta dose">
     
-        <div class="btn-nav">
+        <button class="mobile" on:click={toggleMenu}>
+            <span class="nav-style">menu</span>
+            <img src={arrowSvg} alt="">
+        </button>
+        <div class="btn-nav desktop-nav">
             <button class="nav-style underline" on:click={() => goto('about')}>à propos</button>
             <button class="nav-style underline" on:click={() => goto('gamePresentation')}>Le jeu</button>
             <button class="nav-style encadre" on:click={() => goto('gameRessources')}>ressources</button>
         </div>
     </div>
-    <hr>
+    <hr class="ligne">
+    <nav class:open={isMenuOpen}>
+        <div class="btn-nav mobile-nav" style="display: {isMenuOpen ? 'flex' : 'none'};">
+            <button class="nav-style" on:click={() => goto('about')}>à propos</button>
+            <hr>
+            <button class="nav-style" on:click={() => goto('gamePresentation')}>Le jeu</button>
+            <hr>
+            <button class="nav-style" on:click={() => goto('gameRessources')}>ressources</button>
+            <hr>
+        </div>
+    </nav>
 </header>
