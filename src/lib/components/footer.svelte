f32338c739c7bacf4012f7e80e6ad32bad2965ee
Viturna <viturnapro@gmail.com>
2024-04-08 10:33:38 +0200

    init footer

---
+++ src/lib/components/footer.svelte
@@ -0,0 +1,37 @@
+<style>
+  .flex-header{
+      background-color: var(--white);
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      padding: 10px 5vw;
+  }
+
+  hr{
+      width: 90%;
+      border: 1px solid var(--black);
+  }
+
+  .nav-style{
+      background: none;
+      border: none;
+  }
+</style>
+
+<script>
+  import { goto } from "$app/navigation";
+  import '$lib/styles/styles.css'
+</script>
+
+<footer>
+  <div class="flex-header">
+      <img src="/static/images/logo.svg" alt="logo dose ta dose">
+
+      <div>
+          <button class="nav-style" on:click={() => goto('about')}>A propos</button>
+          <button class="nav-style" on:click={() => goto('gamePresentation')}>Le jeu</button>
+          <button class="nav-style" on:click={() => goto('gameRessources')}>Les ressources du jeu</button>
+      </div>
+  </div>
+  <hr>
+</footer>
