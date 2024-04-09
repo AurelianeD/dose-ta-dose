6abc00480958de3a9741d0c1ed60e60cf368e1a0
LineCab <line.cabaret@mmibordeaux.com>
2024-04-09 17:04:35 +0200

    ajustement rôle

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -8,19 +8,21 @@
 <Header />
 <div class="contenu">
     <h1>A <span class="encadre">propos</span></h1>
-    <h2>Notre équipe</h2>
-    <ul>
-      <li><span class="bold">Safiya SEDDIKI</span> <br>Rédactrice web SEO & Community manager</li>
-      <li><span class="bold">Joséphine SAINT-YGNAN</span> <br>Chargée de communication & Rédactrice de contenus</li>
-      <li><span class="bold">Camille DAGUIN</span> <br>Chargée de communication & Rédactrice de contenus</li>
-      <li><span class="bold">Hugo MENSAH</span> <br> Graphiste & Créateur de contenus </li>
-      <li><span class="bold">Leily COQUARD MISONO</span> <br>Créatrice de contenus</li>
-      <li><span class="bold">Auréliane DOR</span> <br>Développeur web</li>
-      <li><span class="bold">Line CABARET</span> <br>Développeur web</li>
-      <li><span class="bold">Thomas RIQUIER</span> <br>Développeur web</li>
-      <li><span class="bold">Justine GELIS</span> <br>Designer</li>
-      <li><span class="bold">Noémie NUNEZ</span> <br>Designer</li>
-    </ul>
+    <div class="paragraphe">
+      <h2>Notre équipe</h2>
+      <ul>
+        <li><span class="bold">Safiya SEDDIKI</span> <br>Rédactrice web SEO & Rédactrice de contenus</li>
+        <li><span class="bold">Joséphine SAINT-YGNAN</span> <br>Chargée de communication & Rédactrice de contenus</li>
+        <li><span class="bold">Camille DAGUIN</span> <br>Chargée de communication & Rédactrice de contenus</li>
+        <li><span class="bold">Hugo MENSAH</span> <br> Graphiste & Créateur de contenus </li>
+        <li><span class="bold">Leily COQUARD MISONO</span> <br>Graphiste & Créateur de contenus</li>
+        <li><span class="bold">Auréliane DOR</span> <br>Développeur web</li>
+        <li><span class="bold">Line CABARET</span> <br>Développeur web</li>
+        <li><span class="bold">Thomas RIQUIER</span> <br>Développeur web</li>
+        <li><span class="bold">Justine GELIS</span> <br>Designer</li>
+        <li><span class="bold">Noémie NUNEZ</span> <br>Designer</li>
+      </ul>
+    </div>
     <div class="paragraphe">
         <h2>Notre mission</h2>
         <p>Notre projet découle d'un constat simple : au collège l'alcool est un sujet tabou, et pourtant nombreux sont les élèves concernés !</p>
