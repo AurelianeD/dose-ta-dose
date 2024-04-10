aac8805a65c026e720ef70c3ca31d186319c8a4c
LineCab <line.cabaret@mmibordeaux.com>
2024-04-10 09:36:27 +0200

    soustitre ressource

--- src/routes/ressources/+page.svelte
+++ src/routes/ressources/+page.svelte
@@ -10,6 +10,7 @@
                 <img src="/images/illustration-intestin.svg" alt="">
                 <div class="contenu">
                     <h2>Les effets de l'alcool sur la santé</h2>
+                    <p>Sous titre du thème</p>
                     <Link path="ressources/effetAlcool" textUnderline="S'" text="informer" />
                 </div>
             </div>
@@ -17,6 +18,7 @@
                 <img src="/images/illustration-pression.svg" alt="">
                 <div class="contenu">
                     <h2>Pression sociale et alcool</h2>
+                    <p>Sous titre du thème</p>
                     <Link path="ressources/pressionSociale" textUnderline="S'" text="informer" />
                 </div>
             </div>
@@ -24,6 +26,7 @@
                 <img src="/images/illustration-accident.svg" alt="">
                 <div class="contenu">
                     <h2>Nos conseil</h2>
+                    <p>Sous titre du thème</p>
                     <Link path="ressources/nosConseil" textUnderline="S'" text="informer" />
                 </div>
             </div>
@@ -31,6 +34,7 @@
                 <img src="/images/illustration-aide.svg" alt="">
                 <div class="contenu">
                     <h2>Aide</h2>
+                    <p>Sous titre du thème</p>
                     <Link path="ressources/aide" textUnderline="S'" text="informer" />
                 </div>
             </div>
@@ -72,7 +76,13 @@
     .contenu{
         display: flex;
         flex-direction: column;
-        gap: clamp(10px, 20vw, 25px);
+    }
+
+    p{
+        margin: 8px 0 24px 0;
+        font-weight: 200;
+        font-style: italic;
+        font-size: 16px;
     }
 
 </style>
