64242a6d4b10dede9601fb2c94fd5a766927e821
AurelianeD <aureliane.dor@gmail.com>
2024-04-11 11:13:43 +0200

    page guide atelier : ajout des pdf

--- src/routes/ressources/imprimable/+page.svelte
+++ src/routes/ressources/imprimable/+page.svelte
@@ -3,26 +3,10 @@
   import Link from '$lib/components/Link.svelte';
   import Download from '$lib/components/Download.svelte';
   import '$lib/styles/styles.css';
+  import {getFileSize} from "$lib/helpers";
 
   let fileSizes: Record<string, string | null> = {}; // Define the type explicitly
 
-  async function getFileSize(url: string) {
-    try {
-      const response = await fetch(url);
-      if (response.ok) {
-        const blob = await response.blob();
-        const fileSizeBytes = blob.size;
-        const fileSizeMB = (fileSizeBytes / (1024 * 1024)).toFixed(1); // Convert bytes to megabytes and round to 1 decimal place
-        return fileSizeMB;
-      } else {
-        console.error('Failed to fetch the file:', response.status);
-        return null;
-      }
-    } catch (error) {
-      console.error('Error fetching the file:', error);
-      return null;
-    }
-  }
 
   onMount(async () => {
     const documents = [
