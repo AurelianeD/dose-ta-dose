30f09b36c11a778b2eaec30b582c2702e7e27178
Viturna <viturnapro@gmail.com>
2024-04-11 15:14:46 +0200

    Mentions legales

---
+++ src/routes/mentions-legales/+page.svelte
@@ -0,0 +1,114 @@
+<script>
+	import '$lib/styles/styles.css';
+</script>
+
+<div class="contenu">
+	<h1>Mentions <span class="encadre">légales</span></h1>
+	<div class="paragraphe">
+    <p>
+			L’utilisateur s’engage à lire attentivement les Conditions générales d’utilisation détaillées ci-dessous et à les accepter.<br>
+      Accepter les Conditions générales d’utilisation équivaut à la signature et l’acceptation de l’utilisateur de tous les articles des règlements et conditions mentionnés.
+		</p>
+		<h2>Mentions légales : </h2>
+		<p>
+			Éditeur de contenu :  Département MMI (Métiers du Multimédia et de l'Internet) <br>
+      Mentions légales, d'après la Loi pour la confiance dans l'économie du numérique (LCEN) de <br>
+      Responsable éditorial : Safiya SEDDIKI <br>
+      Adresse :  1 Rue Jacques Ellul, 33800 Bordeaux <br>
+      Adresse e-mail : arnaud.levy@mmibordeaux.com <br>
+      Numéro de téléphone :  +33 5 57 12 20 44 <br>
+      Hébergement : Mmibordeaux
+		</p>
+	</div>
+	<div class="paragraphe">
+		<h2>Conception :</h2>
+		<p>Le groupe d'étudiants MMI, Dose ta dose, composé de :</p>
+      <ul>
+        <li>Auréliane DOR</li>
+        <li>Camille DAGUIN</li>
+        <li>Hugo MENSAH</li>
+        <li>Joséphine SAINT-YGNAN</li>
+        <li>Justine GELIS</li>
+        <li>Leïly COQUARD MISONO</li>
+        <li>Line CABARET</li>
+        <li>Noémie NUNEZ</li>
+        <li>Safiya SEDDIKI</li>
+        <li>Thomas RIQUIER</li>
+      </ul>
+
+	</div>
+	<div class="paragraphe">
+		<h2>Conditions Générales d’Utilisation (CGU) :</h2>
+		<p>Les présentes Conditions Générales d'Utilisation (CGU) régissent l'utilisation du site Dose ta Dose, ci-après dénommé "le Site".
+    <br>En accédant au site, toute personne s’engage à accepter sans réserve les présentes CGU</p>
+	</div>
+  <div class="paragraphe">
+		<h2>Propriété intellectuelle :</h2>
+		<p>Tous les éléments du Site, incluant les textes, images, logos, vidéos, sont protégés par la loi de la propriété intellectuelle. La reproduction ou l’utilisation sans autorisation préalable est strictement interdite.</p>
+	</div>
+  <div class="paragraphe">
+		<h2>Responsabilité :</h2>
+		<p>L'éditeur de Dose ta Dose fournit des informations exactes et à jour, validées par des professionnels de santé, mais ne saurait être tenu responsable des erreurs ou omissions. L'utilisation du Site se fait sous la responsabilité de l'utilisateur.
+      <br> <br>
+      Tous les liens hypertextes mis en place sur le Site et dirigeant vers d'autres ressources présentes sur Internet sont utilisés aux risques et périls de l'utilisateur, qui en assume la responsabilité. Dose ta Dose décline toute responsabilité quant à l'exactitude des informations contenues dans ces ressources externes en cas de modifications de ces dernières.
+      <br> <br>
+      Dose ta Dose n'est pas responsable des éventuels préjudices causés au matériel informatique de l'utilisateur suite à son utilisation ou son accès au site.</p>
+	</div>
+  <div class="paragraphe">
+		<h2>Données personnelles :</h2>
+		<p>Aucune donnée personnelle n'est collectée sur le Site. L'utilisateur peut naviguer sur le Site sans avoir à fournir de données personnelles.
+      <br><br>
+      Rédigées à Bordeaux, le 10/04/2024.</p>
+	</div>
+</div>
+
+<style>
+	.contenu {
+		max-width: 60vw;
+		margin: auto;
+		margin-top: 50px;
+		display: flex;
+		flex-direction: column;
+		gap: 40px;
+	}
+
+	p {
+		line-height: 140%;
+	}
+
+	.encadre {
+		background-color: var(--yellow);
+		padding: 0 5px;
+	}
+
+	.paragraphe {
+		display: flex;
+		flex-direction: column;
+		gap: 20px;
+		max-width: 40vw;
+	}
+
+	li {
+		margin: 20px 40px;
+		line-height: 140%;
+		list-style-type: disc;
+		font-family: 'DM Sans', sans-serif;
+	}
+
+	.bold {
+		font-weight: 600;
+	}
+
+	@media screen and (max-width: 1000px) {
+		.contenu {
+			max-width: 70vw;
+		}
+
+		.paragraphe {
+			max-width: 80vw;
+		}
+    li {
+		margin: 20px 10px;
+	}
+	}
+</style>
